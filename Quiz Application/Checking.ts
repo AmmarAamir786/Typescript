b61bf7328a68d0b3338fe21cffb2f2f74fc32677
Ammar Aamir <139608505+AmmarAamir786@users.noreply.github.com>
2023-08-18 16:48:33 +0500

    Add files via upload

---
+++ Quiz Application/Checking.ts
@@ -0,0 +1,15 @@
+import { youranswers, correctanswers } from "./main.js";
+import chalk from "chalk";
+
+let numbers = 0;
+
+for (let i = 0; i < youranswers.length; i++) {
+  if (youranswers[i] === correctanswers[i]) {
+    numbers++;
+  }
+}
+
+console.log(chalk.bgMagenta(`Your score: ${numbers} out of ${correctanswers.length}`));
+
+console.log(chalk.bgGrey(`Your answers: ${youranswers}`));
+console.log(chalk.bgGreen(`Correct answers: ${correctanswers}`));
---
+++ Quiz Application/main.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let quiz = await inquirer.prompt([
+  {
+    name: "Question1",
+    type: "rawlist",
+    choices: [`Ammar`, `Saad`, `Usman`, `Ali`],
+    message: chalk.bgCyan("What is your name?"),
+  },
+  {
+    name: "Question2",
+    type: "rawlist",
+    choices: [`Islamabad`, `Rawat`, `Pindi`, `Gujar Khan`],
+    message: chalk.bgBlackBright("Where do you live?"),
+  },
+  {
+    name: "Question3",
+    type: "rawlist",
+    choices: [`Pink`, `Red`, `Blue`, `Black`],
+    message: chalk.bgBlueBright("What is your favorite color?"),
+  },
+]);
+
+let youranswers = [quiz.Question1, quiz.Question2, quiz.Question3];
+let correctanswers = ["Ammar", "Rawat", "Black"];
+export { youranswers, correctanswers };
